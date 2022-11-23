02976bd95a40b253518e28501a6178686e070a8a
shitori <antonyfourchualt@gmail.com>
2022-11-23 14:07:16 +0100

    purge code gameoflife

--- routes/other.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var router = express.Router()
-
-const controller = require('../controllers/defaultController')
-
-router.get('/', controller.game)
-
-module.exports = router
